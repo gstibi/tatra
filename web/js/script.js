375c00608418a0a9171eae41481d5224e7f83506
gstibi <58399636+gstibi@users.noreply.github.com>
2019-12-19 17:36:31 +0100

    Add files via upload

--- web/js/script.js
+++ web/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   // Transition effect for navbar 
   $(window).scroll(function() {
-    // checks if window is scrolled more than 500px, adds/removes solid class
-    if($(this).scrollTop() > 200) { 
-        $('.navbar').addClass('solid');
+    // checks if window is scrolled more than 100px, adds/removes solid class
+    if($(this).scrollTop() > 100) { 
+        $('.main-menu').addClass('solid');
     } else {
-        $('.navbar').removeClass('solid');
+        $('.main-menu').removeClass('solid');
     }
   });
 });
