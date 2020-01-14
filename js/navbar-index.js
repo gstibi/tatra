fdb3c1557adb1ae01289d4467e2f586d0ae7c281
gstibi <58399636+gstibi@users.noreply.github.com>
2020-01-14 22:43:42 +0100

    Add files via upload

---
+++ js/navbar-index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    // Transition effect for navbar 
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 100px, adds/removes solid class
+      if($(this).scrollTop() > 100) { 
+          $('.main-menu').addClass('solid');
+          document.getElementById("logo").src="img/222x68.png";
+          $('.main-menu').removeClass('topnav');
+          $('.logo').addClass('change-size');
+          $('.navbar-nav').addClass('fontcolor');
+      } else {
+          $('.main-menu').removeClass('solid');
+          document.getElementById("logo").src="img/222x68-color.png";
+          $('.main-menu').addClass('topnav');
+          $('.logo').removeClass('change-size');
+          $('.navbar-nav').removeClass('fontcolor');
+      }
+    
+    });
+  });
--- js/script.js
+++ js/script.js
@@ -2,12 +2,26 @@ $(document).ready(function() {
   // Transition effect for navbar 
   $(window).scroll(function() {
     // checks if window is scrolled more than 100px, adds/removes solid class
-    if($(this).scrollTop() > 500) { 
+    if($(this).scrollTop() > 100) { 
         $('.main-menu').addClass('solid');
         document.getElementById("logo").src="img/222x68.png";
+        $('.logo').addClass('change-size');
+        $('.navbar-nav').addClass('fontcolor');
     } else {
         $('.main-menu').removeClass('solid');
         document.getElementById("logo").src="img/222x68-color.png";
+        $('.logo').removeClass('change-size');
+        $('.navbar-nav').removeClass('fontcolor');
+    }
+
+    //zistuje ci si na index.html --> dat pozor pri deployment alebo dat do noveho scriptu
+    var href = document.location.href;
+    console.log(href)
+    var lastPathSegment = href.substr(href.lastIndexOf('/') + 1);
+    if($(this).scrollTop() > 100 && lastPathSegment =='index.html'){
+      $('.main-menu').addClass('topnav');
+    } else {
+      $('.main-menu').removeClass('topnav');
     }
   
   });
